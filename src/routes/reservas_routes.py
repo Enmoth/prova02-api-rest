d220216d5f3358ac30cf5fac9c67d2e10c714541
Jean <jtrojann@gmail.com>
2023-11-27 20:16:31 -0300

    Primeiro e ultimo

--- src/routes/reservas_routes.py
+++ src/routes/reservas_routes.py
@@ -1,6 +1,6 @@
 import random
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from sqlmodel import select
 
@@ -18,7 +18,7 @@ def lista_reservas_voo(id_voo: int):
         reservas = session.exec(statement).all()
         return reservas
 
-
+# RESERVA
 @reservas_router.post("")
 def cria_reserva(reserva: Reserva):
     with get_session() as session:
@@ -29,9 +29,22 @@ def cria_reserva(reserva: Reserva):
                 content={"message": f"Voo com id {reserva.voo_id} não encontrado."},
                 status_code=404,
             )
+        
+        # ---
+        # Verificar se já existe uma reserva com o mesmo documento para o mesmo voo
+        reserva_existente = (
+            session.query(Reserva)
+            .filter(Reserva.voo_id == reserva.voo_id, Reserva.documento == reserva.documento)
+            .first()
+        )
 
-        # TODO - Validar se existe uma reserva para o mesmo documento
-
+        # Retorna uma exception caso exista
+        if reserva_existente:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Já existe uma reserva com o documento {reserva.documento} para este voo.",
+            )
+        # ---
         codigo_reserva = "".join(
             [str(random.randint(0, 999)).zfill(3) for _ in range(2)]
         )
@@ -42,10 +55,85 @@ def cria_reserva(reserva: Reserva):
         session.refresh(reserva)
         return reserva
 
-
+# CHECKIN
 @reservas_router.post("/{codigo_reserva}/checkin/{num_poltrona}")
 def faz_checkin(codigo_reserva: str, num_poltrona: int):
-    # TODO - Implementar reserva de poltrona
-    pass
+    with get_session() as session:
 
-# TODO - Implementar troca de reserva de poltrona
+        reserva = (
+            session.query(Reserva)
+            .filter(Reserva.codigo_reserva == codigo_reserva)
+            .first()
+        )
+
+        if not reserva:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Reserva com código {codigo_reserva} não encontrada.",
+            )
+        
+        # Valida se a poltrona existe, com base no model de voos
+        if num_poltrona < 1 or num_poltrona > 9:
+            raise HTTPException(
+                status_code=400,
+                detail="Número de poltrona inválido. Deve ser um número entre 1 e 9.",
+            )
+
+        # Verificar se a poltrona está disponível
+        poltrona_field = f"poltrona_{num_poltrona}"
+        if getattr(reserva.voo, poltrona_field):
+            raise HTTPException(
+                status_code=400,
+                detail=f"Poltrona {num_poltrona} já reservada para este voo.",
+            )
+
+        # Marcar a poltrona como reservada
+        setattr(reserva.voo, poltrona_field, reserva.documento)
+        session.commit()
+
+        return {"message": f"Check-in realizado para a poltrona {num_poltrona} com sucesso."}
+
+
+# TROCA DE POLTRONAS
+@reservas_router.patch("/{codigo_reserva}/troca/{poltrona_origem}/{poltrona_destino}")
+def troca_reserva_poltrona_patch(codigo_reserva: str, poltrona_origem: int, poltrona_destino: int):
+    with get_session() as session:
+        reserva = (
+            session.query(Reserva)
+            .filter(Reserva.codigo_reserva == codigo_reserva)
+            .first()
+        )
+
+        if not reserva:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Reserva com código {codigo_reserva} não encontrada.",
+            )
+
+        if poltrona_origem < 1 or poltrona_origem > 9 or poltrona_destino < 1 or poltrona_destino > 9:
+            raise HTTPException(
+                status_code=400,
+                detail="Número de poltrona inválido. Deve ser um número entre 1 e 9.",
+            )
+
+        poltrona_origem_field = f"poltrona_{poltrona_origem}"
+        poltrona_destino_field = f"poltrona_{poltrona_destino}"
+
+        if getattr(reserva.voo, poltrona_origem_field) != reserva.documento:
+            raise HTTPException(
+                status_code=400,
+                detail=f"Você só pode trocar uma poltrona que está reservada para você. Poltrona {poltrona_origem} não está reservada para esta reserva.",
+            )
+
+        if getattr(reserva.voo, poltrona_destino_field):
+            raise HTTPException(
+                status_code=400,
+                detail=f"Poltrona {poltrona_destino} já reservada para este voo.",
+            )
+
+        setattr(reserva.voo, poltrona_origem_field, None)
+        setattr(reserva.voo, poltrona_destino_field, reserva.documento)
+        session.commit()
+
+        return {"message": f"Troca de poltronas realizada com sucesso: {poltrona_origem} para {poltrona_destino}."}
+
